73b044498b8c2ff871adddaeee749f3297043129
GuillermoBaldan <guillermobaldan@gmail.com>
2023-03-28 21:36:27 +0200

    update Category data

---
+++ updateCategory.php
@@ -0,0 +1,22 @@
+<?php
+// Conexión a la base de datos
+require 'db_config.php';
+
+// Obtener los datos enviados desde el formulario
+$id_categoria = $_POST['id_categoria'];
+$nombre_categoria = $_POST['nombre_categoria'];
+
+// Preparar y ejecutar la consulta para actualizar la categoría
+$sql = "UPDATE Categoria SET nombre_categoria = ? WHERE id_categoria = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("si", $nombre_categoria, $id_categoria);
+
+if ($stmt->execute()) {
+    header("Location: successPage.html"); // Redireccionar a una página de éxito
+} else {
+    header("Location: errorPage.html"); // Redireccionar a una página de error
+}
+
+$stmt->close();
+$conn->close();
+?>
