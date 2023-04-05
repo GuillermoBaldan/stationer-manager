2e8d994ab6a22eef08854b7ff8864c6a62de9708
GuillermoBaldan <guillermobaldan@gmail.com>
2023-04-05 05:10:27 +0200

    findCategory.php modified

--- findCategory.php
+++ findCategory.php
@@ -1,10 +1,8 @@
 <?php
 // Conexión a la base de datos
 require 'db_config.php';
-
 // Obtener datos del formulario
 $palabra_busqueda = isset($_POST['palabra']) ? $_POST['palabra'] : '';
-
 // Consultar la tabla 'Categoria' para buscar categorías por nombre
 $sql = "SELECT * FROM Categoria WHERE nombre_categoria LIKE ?";
 $stmt = $conn->prepare($sql);
@@ -12,7 +10,6 @@ $search_param = "%" . $palabra_busqueda . "%";
 $stmt->bind_param("s", $search_param);
 $stmt->execute();
 $result = $stmt->get_result();
-
 // Generar tabla HTML con los resultados
 echo "<!DOCTYPE html>
 <html lang='es'>
@@ -24,7 +21,6 @@ echo "<!DOCTYPE html>
 </head>
 <body>";
 include 'components/navbar.php';
-
 echo "
     <h1>Buscar Categorías</h1>
     <form method='POST' action='findCategory.php'>
@@ -32,7 +28,6 @@ echo "
         <input type='text' id='palabra' name='palabra' value='" . $palabra_busqueda . "' required>
         <button type='submit'>Buscar</button>
     </form>";
-
 if ($palabra_busqueda != '') {
     if ($result->num_rows > 0) {
         echo "<table id='resultTable' border='1'>
@@ -44,105 +39,42 @@ if ($palabra_busqueda != '') {
                     </tr>
                 </thead>
                 <tbody>";
-
                 while ($row = $result->fetch_assoc()) {
                     echo "<tr data-id='" . $row['id_categoria'] . "' onclick='showArticulos(" . $row['id_categoria'] . ")'>
                             <td>" . $row['id_categoria'] . "</td>
                             <td>" . $row['nombre_categoria'] . "</td>
                             <td>
                                 <button onclick='event.stopPropagation(); updateCategory(" . $row['id_categoria'] . ")'>Actualizar</button>
-                                <button onclick='event.stopPropagation(); deleteCategory(" . $row['id_categoria'] . ")'>Eliminar</button>
+                                <button onclick='event.stopPropagation(); showArticulosByCategory(" . $row['id_categoria'] . ")'>Eliminar</button>
                             </td>
                           </tr>";
                 }
                 
-
         echo "</tbody></table>";
     } else {
         echo "<p>No se encontraron resultados para la búsqueda.</p>";
     }
 }
-
 echo "<script>
-function updateCategory(id_categoria) {
-    window.location.href = 'updateCategory.html?id_categoria=' + id_categoria;
-}
-
-function deleteCategory(id_categoria) {
-    if (confirm('¿Está seguro de que desea eliminar esta categoría?')) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'deleteCategory.php?id_categoria=' + id_categoria, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                try {
-                    let response = JSON.parse(xhr.responseText);
-                    if (response.error) {
-                        alert(response.error);
-                        showArticulosRelacionados(response.articulos);
-                    } else {
-                        alert('La categoría se eliminó con éxito.');
-                        location.reload();
-                    }
-                } catch (e) {
-                    console.error(e);
-                }
+function showArticulosByCategory(id_categoria) {
+    fetch(`getItemsByCategory.php?id_categoria=` + id_categoria)
+        .then(response => response.json())
+        .then(data => {
+            if (data.articulos.length > 0) {
+                console.log('Artículos en la categoría:', data.articulos);
+            } else {
+                console.log('No hay artículos en esta categoría.');
+                console.log(data);
             }
-        };
-        xhr.send();
-    }
+        })
+        .catch(error => console.error('Error:', error));
 }
 
-function showArticulosRelacionados(articulos) {
-    let table = document.createElement('table');
-    table.border = '1';
-    table.innerHTML = '<thead><tr><th>ID Artículo</th><th>Nombre Artículo</th><th>Descripción</th><th>Precio</th></tr></thead><tbody></tbody>';
-    let tbody = table.querySelector('tbody');
-    let example = 'hola';
-    for (let i = 0; i < articulos.length; i++) {
-        let tr = document.createElement('tr');
-        tr.innerHTML = `<td>` + '$' + `{example}</td>`;
-        tr.innerHTML = `<td>` + '$' `{articulos[i].id_articulo}</td><td>`+ '$' + `{articulos[i].nombre_articulo}</td><td>`+'$'+`{articulos[i].descripcion}</td><td>` + '$' + `{articulos[i].precio}</td>`;
-        tbody.appendChild(tr);
-    }
-    let container = document.createElement('div');
-    container.innerHTML = `
-        <h2>Artículos relacionados</h2>
-        <p>Por favor, elimine los siguientes artículos antes de eliminar la categoría:</p>
-    `;
-    container.appendChild(table);
-
-    let resultTable = document.getElementById('resultTable');
-    if (resultTable) {
-        resultTable.parentNode.insertBefore(container, resultTable.nextSibling);
-    } else {
-        document.body.appendChild(container);
-    }
-}
-
-function showArticulos(id_categoria) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'getItemsByCategory.php?id_categoria=' + id_categoria, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            try {
-                let response = JSON.parse(xhr.responseText);
-                if (response.error) {
-                    alert(response.error);
-                } else {
-                    showArticulosRelacionados(response.articulos);
-                }
-            } catch (e) {
-                console.error(e);
-            }
-        }
-    };
-    xhr.send();
-}
 </script>
 ";
-    
+
     echo "</body>
-    
+
     </html>";
     include 'components/mainMenuButton.php';
     $stmt->close();
--- getItemsByCategory.php
+++ getItemsByCategory.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type: application/json');
 require 'db_config.php';
 
 if (isset($_GET['id_categoria'])) {
@@ -14,12 +15,16 @@ if (isset($_GET['id_categoria'])) {
     while ($row = $result->fetch_assoc()) {
         $articulos[] = $row;
     }
-
+    
     $stmt->close();
     $conn->close();
-
+    
+    //echo "El id mandando es ".$id_categoria
     echo json_encode(['articulos' => $articulos]);
+    //echo json_encode(['articulos' => $_GET['id_categoria']]);
+    // Eliminada la línea innecesaria de "echo" aquí
 } else {
     echo json_encode(['error' => 'No se proporcionó el ID de la categoría.']);
-}
-?>
+    // Eliminada la línea innecesaria de "echo" aquí
+    }
+    ?>
